1e4ace88018617076d565434e5728127bb67233f
Young-Jae <youngjae7981@gmail.com>
2023-03-08 11:29:24 +0900

    버튼 사리지는거

--- nativeapp/src/component/CommentList.jsx
+++ nativeapp/src/component/CommentList.jsx
@@ -1,44 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import Comment from "./Comment";
 
 const comments = [
   {
     name: "김영재1",
-    comment: "안녕하세요. 김영재 입니다.",
+    comment: "나는야 김영재.",
   },
   {
-    name: "김영재2",
-    comment: "리액트 재밌다~!",
+    name: "김영재 2",
+    comment: "리액트는 재밌어요",
   },
   {
-    name: "김영재3",
-    comment: "리액트 재미없어!!",
+    name: "김영재 3",
+    comment: "노잼",
   },
 ];
 
-function App() {
-  const [comments, setComments] = useState([]);
+function CommentList() {
+  const [commentList, setCommentList] = useState(comments);
 
-  const handleRemoveComment = (idToRemove) => {
-    const updatedComments = comments.filter(
-      (comment) => comment.id !== idToRemove
-    );
-    setComments(updatedComments);
+  const deleteComment = (index) => {
+    const newCommentList = [...commentList];
+    newCommentList.splice(index, 1);
+    setCommentList(newCommentList);
   };
 
   return (
     <div>
-      {comments.map((comment) => (
-        <Comment
-          key={comment.id}
-          id={comment.id}
-          name={comment.name}
-          comment={comment.comment}
-          onRemove={handleRemoveComment}
-        />
-      ))}
+      {commentList.map((comment, i) => {
+        return (
+          <Comment
+            name={comment.name}
+            comment={comment.comment}
+            key={i}
+            onDelete={() => deleteComment(i)}
+          />
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+export default CommentList;
